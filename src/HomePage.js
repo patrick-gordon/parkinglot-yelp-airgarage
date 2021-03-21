baed98569e7ff67772985eb820efdc50035c6fe1
Patrick Gordon <48340286+patrick-gordon@users.noreply.github.com>
2021-03-20 20:47:44 -0400

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,11 +5,13 @@ import ParkingLots from './ParkingLots';
 import './HomePage.css'
 
 export default function HomePage() {
+  // initialize as empty array 
   const [data, setData] = useState([]);
+  // initialize as empty sting for user input
   const [location, setLocation] = useState('');
   let token =
     'mi5qSSqdhmrNXBjLq5MBMwuqcS0q8aE4u52fwqrG8CkrBjjksgdV8ZblHdh4ThtDqQVFapfOwrCqadcTH4sJIMhQgEcWpc0bK_9ms_rJ1H-xMT1Amp4tmH_PhAg3X3Yx';
-
+  // search function used to submit user input and make axios call
   const search = (e) => {
     axios
       .get(
@@ -29,22 +31,29 @@ export default function HomePage() {
       .then((res) => {
         // console.log(res.data.businesses);
         // setData(res.data.businesses);
-        let newData = (res.data.businesses)
-        console.log(newData)
-        let newData1 = [...newData].reverse()
-        console.log(newData1)
-        setData(newData1)
+        let newData = (res.data.businesses) // set data from axios call
+        //console.log(newData)
+        let newData1 = [...newData].reverse() // spread in newdata and reverse the array
+        //console.log(newData1)
+        setData(newData1) // connect newly reverse array to state using the useState hook
 
       })
       .catch((err) => {
         console.log(err.message);
       });
   };
-
+  // handle input change
   const handleChange = (e) => {
     console.log(location);
     setLocation(e.target.value);
   };
+  // Use ENTER key to submit user input
+  const onKeyPress = (e) => {
+    if (e.which === 13) {
+      search();
+    }
+  };
+  
   return (
     <Container className='homepage-container' >
     <h2>Search for Parking Lots</h2>
@@ -56,6 +65,7 @@ export default function HomePage() {
         onChange={handleChange}
         placeholder='Where would you like to search?'
         size='30'
+        onKeyPress={onKeyPress}
       ></input>
       <Button onClick={search}>Search</Button>
     </div>
